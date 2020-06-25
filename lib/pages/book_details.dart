ab6d760bd371ab964a37a7449abb26227067dfe9
Rahul Babu <rahulblatz002@gmail.com>
2020-06-25 23:57:09 +0530

    Need to Animate Stuffs

--- lib/pages/book_details.dart
+++ lib/pages/book_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class BookDetailsPage extends StatelessWidget {
   @override
@@ -7,18 +8,41 @@ class BookDetailsPage extends StatelessWidget {
       child: Scaffold(
         body: Stack(
           children: <Widget>[
-            Hero(
-              tag: 'book1',
-              child: Container(
-                height: 400,
-                width: MediaQuery.of(context).size.width,
-                child: Image.asset(
-                  'assets/images/book2.png',
-                  fit: BoxFit.fill,
-                ),
-              ),
-            )
+            buildHero(context),
+            buildBackArrow(context),
           ],
+        ),
+      ),
+    );
+  }
+
+  Padding buildBackArrow(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0),
+      child: Positioned(
+        child: IconButton(
+          icon: Icon(
+            Ionicons.ios_arrow_round_back,
+            size: 35.0,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+
+  Hero buildHero(BuildContext context) {
+    return Hero(
+      tag: 'book1',
+      child: Container(
+        height: 400,
+        width: MediaQuery.of(context).size.width,
+        child: Image.asset(
+          'assets/images/book2.png',
+          fit: BoxFit.fill,
         ),
       ),
     );
