4a987914dbe866dfaf637090e9618c1075230d7c
Rahul Babu <rahulblatz002@gmail.com>
2020-06-26 11:24:22 +0530

    Added Buttons to the details page

--- lib/pages/book_details.dart
+++ lib/pages/book_details.dart
@@ -1,3 +1,5 @@
+import 'package:bookstore/constants.dart';
+import 'package:bookstore/widgets/fancy_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 
@@ -6,11 +8,87 @@ class BookDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            buildHero(context),
-            buildBackArrow(context),
-          ],
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.only(
+            bottom: 20.0,
+          ),
+          child: Container(
+            color: Colors.grey[200],
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    FancyButton(
+                      content: 'Want to read',
+                      icon: AntDesign.down,
+                      bgColor: Color(primaryRed),
+                      shadowColor: Color(primaryRed),
+                      iconColor: Colors.white,
+                      textColor: Colors.white,
+                    ),
+                    FancyButton(
+                      content: 'Get a copy',
+                      icon: MaterialCommunityIcons.book_outline,
+                      bgColor: Colors.white,
+                      shadowColor: Colors.grey,
+                      iconColor: Color(primaryRed),
+                      textColor: Colors.black,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                buildHero(context),
+                Container(
+                  color: Colors.grey[200],
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height - 400,
+                ),
+//            Stack(
+//              children: <Widget>[
+//                buildBackArrow(context),
+//                Positioned(
+//                  bottom: 0,
+//                  child: buildAuthorIcon(),
+//                ),
+//              ],
+//            ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildAuthorIcon() {
+    return Container(
+      width: 65,
+      height: 65,
+      decoration: BoxDecoration(
+        border: Border.all(width: 2, color: Colors.grey[350]),
+        image: DecorationImage(
+          image: AssetImage(
+            'assets/images/person4.jpg',
+          ),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: BorderRadius.all(
+          Radius.circular(10.0),
         ),
       ),
     );
@@ -38,7 +116,7 @@ class BookDetailsPage extends StatelessWidget {
     return Hero(
       tag: 'book1',
       child: Container(
-        height: 400,
+        height: 500,
         width: MediaQuery.of(context).size.width,
         child: Image.asset(
           'assets/images/book2.png',
---
+++ lib/widgets/fancy_button.dart
@@ -0,0 +1,65 @@
+import 'package:bookstore/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FancyButton extends StatelessWidget {
+  final String content;
+  final IconData icon;
+  final Color iconColor, bgColor, textColor, shadowColor;
+
+  FancyButton(
+      {this.content,
+      this.icon,
+      this.bgColor,
+      this.iconColor,
+      this.shadowColor,
+      this.textColor});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 11.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(15.0),
+          border: Border.all(
+            width: 1,
+            color: Colors.grey.withOpacity(.5),
+          ),
+          boxShadow: [
+            BoxShadow(color: shadowColor.withOpacity(.8), blurRadius: 3),
+          ],
+        ),
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 8.0),
+              child: Text(
+                '$content',
+                style: GoogleFonts.quicksand(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w500,
+                  color: textColor,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 15.0),
+              child: Icon(
+                icon,
+                color: iconColor,
+                size: 17.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
